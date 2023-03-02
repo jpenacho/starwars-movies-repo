344f664bfe7864f121614119920c319ab9724bd5
jpenacho <jpenacho@gmail.com>
2023-03-02 13:52:39 +0000

    Service Skeleton

---
+++ src/main/java/com/jpenacho/starwarsmoviesrepo/StarWarsMoviesRepoApplication.java
@@ -0,0 +1,13 @@
+package com.jpenacho.starwarsmoviesrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsMoviesRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsMoviesRepoApplication.class, args);
+	}
+
+}
